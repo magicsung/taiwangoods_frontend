8556c0f4c4868c2dc5fd08d8ec007ec2b1c108ed
Jack Sung <magicsung@gmail.com>
2016-03-10 15:46:50 +0800

    Add left menu.

---
+++ js/main.js
@@ -0,0 +1,57 @@
+$( document ).ready(function() {
+
+  // menu collapse
+  var openMenuAction = function(event) {
+    event.preventDefault();
+    $('#menu-toggle').css("display", "block");
+    $('#menu-content').animate({
+      left: "0"
+    }, 300);
+  };
+  var closeMenuAction = function(event){
+    event.preventDefault();
+    $('#menu-content').animate({
+      left: "-75%"
+    }, 300, function() {
+        $('#menu-toggle').css("display", "none");
+    });
+  }
+
+  var openMenu = document.getElementById('open-menu');
+  openMenu.addEventListener('touchstart', openMenuAction);
+  openMenu.addEventListener('click', openMenuAction);
+
+  var closeMenu = document.getElementById('close-menu');
+  closeMenu.addEventListener('touchstart', closeMenuAction);
+  closeMenu.addEventListener('click', closeMenuAction);
+
+  // menu-item collapse
+  var menuItemClickAction = function(event){
+    event.preventDefault();
+    $(this).siblings().slideToggle(200, function(){
+      $(this).siblings().toggleClass('active')
+        .children('span').toggleClass('glyphicon-chevron-down').toggleClass('glyphicon-chevron-up');
+      $(this).parent().siblings().children('.menu-sub-items').slideUp(200, function(){
+        $(this).siblings().removeClass('active')
+          .children('span').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
+      });
+    });
+  }
+
+  $('.menu-link').click(menuItemClickAction);
+  var menuLink = document.getElementsByClassName('menu-link');
+  for (var i = 0; i < menuLink.length; i++) {
+    menuLink[i].addEventListener('touchstart', menuItemClickAction);
+  }
+
+
+  // touch link loading
+  var linkLoading = document.getElementsByClassName('menu-link');
+  for (var i = 0; i < linkLoading.length; i++) {
+    linkLoading[i].addEventListener('touchstart', function() {
+      $(this).toggleClass('load');
+    });
+  }
+
+
+});

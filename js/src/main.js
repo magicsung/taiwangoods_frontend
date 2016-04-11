a91ec013dd22d3e5fe6f977d9edc8acfcf77b972
Jack Sung <magicsung@gmail.com>
2016-04-11 15:20:24 +0800

    add npm script. use uglify js & node sass.

--- js/src/main.js
+++ js/src/main.js
@@ -1,5 +1,21 @@
 $( document ).ready(function() {
 
+  // scroll smooth
+  $('a[href="#top"]').click(goTop);
+  function goTop() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      console.log(target);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 400);
+        return false;
+      }
+    }
+  };
+
   // scroll page
   var windowWidth = $(window).width();
   if (windowWidth < 768) {
